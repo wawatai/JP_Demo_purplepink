3eea2779a702d583620b55a98ad036c4580799ff
wawatai <wataiPublic@gmail.com>
2022-09-06 18:32:58 +0800

    111-09-06

--- js/js.jsx
+++ js/js.jsx
@@ -9,3 +9,57 @@ $(document).on('ready', function() {
 		arrows: false,
 	})
 })
+
+//gwrapBox
+$(function(){
+    $(".gWrapBox .gameWrap").each(function(){
+        if($(this).find("li").length > 2)
+        {
+            $(".nextBtn")
+            .addClass("active");
+        }
+    })
+
+    var hotN = 0;
+    var hotN = 0;
+
+    $(".nextBtn").on("click",function(){
+        var total = $(this).prev(".gWrapBox").find("li").length;
+        var n = Math.round((total - 3));
+        
+        hotN ++;
+
+        $(".gWrapBox .gameWrap")
+        .css("transform","translateX("+ (-110 * hotN) +"px)");
+
+        if(hotN == n)
+        {
+            hotN == n;
+
+            $(this)
+            .removeClass("active");
+        }
+    })
+})
+
+
+//backTOtop
+$(function(){
+	$(window).scroll(function () {
+        var scrollVal = $(this).scrollTop();
+        if(scrollVal > 0){
+            $(".goTop")
+            .addClass("display")
+        } else{
+            $(".goTop")
+            .removeClass("display");
+        };
+	})
+
+    $(".goTop").click(function(){
+        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body'); //各瀏覽器相容性
+        $body.delay('0').animate({
+            scrollTop: 0
+        },500)
+    })
+})
